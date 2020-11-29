78e1d88ad6b67d368291b008efbab3b05012be25
Hareds567 <60515014+Hareds567@users.noreply.github.com>
2020-11-29 01:17:25 -0500

    Final

--- app/src/main/java/com/example/logindemo2/MyCovidData_ReportPositiveTest.java
+++ app/src/main/java/com/example/logindemo2/MyCovidData_ReportPositiveTest.java
@@ -3,6 +3,7 @@ package com.example.logindemo2;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,11 +14,13 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -29,14 +32,12 @@ import org.json.JSONObject;
 
 
 public class MyCovidData_ReportPositiveTest extends Fragment {
+    private final String get_isAllowed = "https://covidtrackerdev.herokuapp.com/get_allowed_to_report";
+    private final String post_self_report = "https://covidtrackerdev.herokuapp.com/post_self_report";
 
-    private final String getSocialURL = "https://covidtrackerdev.herokuapp.com/testcsv";
     private final String sendAlertSocial = "https://covidtrackerdev.herokuapp.com/post_send_alert";
     private final String sendAlertClass = "https://covidtrackerdev.herokuapp.com/post_class_alert";
     private final String sendAlertWorkplace = "https://covidtrackerdev.herokuapp.com/post_alert_workplace";
-
-
-    private TextView displayResidence_TextView;
 
     //==============================================================================================
     //Google Sign in
@@ -52,6 +53,7 @@ public class MyCovidData_ReportPositiveTest extends Fragment {
     private Button Confirm;
     private String Res;
     private RadioGroup options;
+    private Boolean success;
 
     public MyCovidData_ReportPositiveTest() {
     }
@@ -69,6 +71,7 @@ public class MyCovidData_ReportPositiveTest extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View rootView = inflater.inflate(R.layout.fragment_my_covid_data_report_positive_test, container, false);
+        success = false;
         //=========================================================================================
         //Processes the google login
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
@@ -89,9 +92,28 @@ public class MyCovidData_ReportPositiveTest extends Fragment {
         options = rootView.findViewById(R.id.radioGroup);
 
         //==========================================================================================
-        //Get new test results
-
-
+        //Get new test results availability
+        JSONObject allowed = new JSONObject();
+        try {
+            allowed.put("ReportUser", userGmail);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String requestAllowNewResult = allowed.toString();
+        BooleanRequest booleanRequest = new BooleanRequest(Request.Method.POST, get_isAllowed,requestAllowNewResult, new Response.Listener<Boolean>(){
+            @Override
+            public void onResponse(Boolean response) {
+                System.out.println("First Test Result Response: " + response);
+                success = response;
+                System.out.println("Inside Boolean Request: "+ success);
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("Error: " + error);
+            }
+        });
+        queue.add(booleanRequest);
 
         //==========================================================================================
         //React to buttons
@@ -103,58 +125,70 @@ public class MyCovidData_ReportPositiveTest extends Fragment {
                 Confirm.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        if(checkedId == R.id.radioButton3){
-                            JSONObject jsonUserGmail = new JSONObject();
-                            try {
-                                jsonUserGmail.put("Email",userGmail);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
+
+
+                            if (checkedId == R.id.radioButton3) {
+                                if(success) {
+                                    JSONObject jsonUserGmail = new JSONObject();
+                                    try {
+                                        jsonUserGmail.put("Email", userGmail);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                    //Post request to notify user's to user's social circle
+                                    JsonObjectRequest requestAlertSocial = new JsonObjectRequest(Request.Method.POST, sendAlertSocial,
+                                            jsonUserGmail, new Response.Listener<JSONObject>() {
+                                        @Override
+                                        public void onResponse(JSONObject response) {
+                                        }
+                                    }, new Response.ErrorListener() {
+                                        @Override
+                                        public void onErrorResponse(VolleyError error) {
+                                            Toast.makeText(getActivity(), "Unable to get Send Alert", Toast.LENGTH_LONG);
+                                        }
+                                    });
+                                    //Post request to notify user's classmates
+                                    JsonObjectRequest requestAlertCourse = new JsonObjectRequest(Request.Method.POST, sendAlertClass,
+                                            jsonUserGmail, new Response.Listener<JSONObject>() {
+                                        @Override
+                                        public void onResponse(JSONObject response) {
+                                        }
+                                    }, new Response.ErrorListener() {
+                                        @Override
+                                        public void onErrorResponse(VolleyError error) {
+                                            Toast.makeText(getActivity(), "Unable to get Send Alert", Toast.LENGTH_LONG);
+                                        }
+                                    });
+                                    //Post request to notify user's coo-workers.
+                                    JsonObjectRequest requestAlertWorkplace = new JsonObjectRequest(Request.Method.POST, sendAlertWorkplace,
+                                            jsonUserGmail, new Response.Listener<JSONObject>() {
+                                        @Override
+                                        public void onResponse(JSONObject response) {
+                                        }
+                                    }, new Response.ErrorListener() {
+                                        @Override
+                                        public void onErrorResponse(VolleyError error) {
+                                            Toast.makeText(getActivity(), "Unable to get Send Alert", Toast.LENGTH_LONG);
+                                        }
+                                    });
+
+                                    queue.add(requestAlertSocial);
+                                    queue.add(requestAlertCourse);
+                                    queue.add(requestAlertWorkplace);
+                                    queue.add(updateSelfReportModel());
+                                    MyCovidData_ReportPositiveTest currentFragment = new MyCovidData_ReportPositiveTest();
+                                    FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                                    fragmentTransaction.replace(R.id.nav_host_fragment, currentFragment,currentFragment.getTag());
+                                    fragmentTransaction.commit();
+                                    Toast.makeText(getActivity(), "Thank You for Submitting Your Test Results!", Toast.LENGTH_LONG);
+                                } else {
+                                    System.out.println("You already submitted a Positive Test Result");
+                                    Toast.makeText(getActivity(), "You already submitted a Positive Test Result", Toast.LENGTH_LONG);
+                                }
+                            } else {
+                                //send neg
                             }
-                            //Post request to notify user's to user's social circle
-                            JsonObjectRequest requestAlertSocial = new JsonObjectRequest(Request.Method.POST, sendAlertSocial,
-                                    jsonUserGmail, new Response.Listener<JSONObject>() {
-                                @Override
-                                public void onResponse(JSONObject response) {
-                                }
-                            }, new Response.ErrorListener() {
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    Toast.makeText(getActivity(),"Unable to get Send Alert",Toast.LENGTH_LONG);
-                                }
-                            });
-                            //Post request to notify user's classmates
-                            JsonObjectRequest requestAlertCourse = new JsonObjectRequest(Request.Method.POST, sendAlertClass,
-                                    jsonUserGmail, new Response.Listener<JSONObject>() {
-                                @Override
-                                public void onResponse(JSONObject response) {
-                                }
-                            }, new Response.ErrorListener() {
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    Toast.makeText(getActivity(),"Unable to get Send Alert",Toast.LENGTH_LONG);
-                                }
-                            });
-                            //Post request to notify user's coo-workers.
-                            JsonObjectRequest requestAlertWorkplace = new JsonObjectRequest(Request.Method.POST, sendAlertWorkplace,
-                                    jsonUserGmail, new Response.Listener<JSONObject>() {
-                                @Override
-                                public void onResponse(JSONObject response) {
-                                }
-                            }, new Response.ErrorListener() {
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    Toast.makeText(getActivity(),"Unable to get Send Alert",Toast.LENGTH_LONG);
-                                }
-                            });
-
-                            queue.add(requestAlertSocial);
-                            queue.add(requestAlertCourse);
-                            queue.add(requestAlertWorkplace);
-
-                        }else {
-                            //send neg
-                        }
-                        Confirm.setVisibility(View.GONE);
+                            Confirm.setVisibility(View.GONE);
                     }
                 });
 
@@ -163,5 +197,24 @@ public class MyCovidData_ReportPositiveTest extends Fragment {
         return rootView;
     }
 
+    private JsonObjectRequest updateSelfReportModel(){
+        JSONObject reportUser = new JSONObject();
+        try {
+            reportUser.put("ReportUser", userGmail);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, post_self_report, reportUser, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
 
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        return request;
+    }
 }
